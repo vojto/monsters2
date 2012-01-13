64fb101e76a77f649eeb0bf2773a9aecb62ae94d
Vojto Rinik <vojto@rinik.net>
2012-01-13 12:12:41 +0100

    Adding docs for rest of the models

--- monsters2/M2CanvasObject.h
+++ monsters2/M2CanvasObject.h
@@ -10,12 +10,44 @@
 #import <CoreData/CoreData.h>
 
 
+/**
+ Represents object on canvas. Contains properties describing applied transformations.
+ */
 @interface M2CanvasObject : NSManagedObject
 
+/****************************************************************************
+ @name Working with object
+ ****************************************************************************/
+
+/**
+ Serialized object.
+ 
+ @see M2Object
+ */
 @property (nonatomic, retain) NSData * object;
+
+/****************************************************************************
+ @name Managing position & transformation
+ ****************************************************************************/
+
+/**
+ Frame of object on canvas. 
+ */
 @property (nonatomic, retain) NSString * frame;
+
+/**
+ Rotation in degrees.
+ */
 @property (nonatomic, retain) NSNumber * rotation;
+
+/**
+ Boolean value indicating whether the object is flipped horizontally.
+ */
 @property (nonatomic, retain) NSNumber * isFlippedHorizontally;
+
+/**
+ Boolean value indicating whether the object is flipped vertically.
+ */
 @property (nonatomic, retain) NSNumber * isFlippedVertically;
 
 @end
--- monsters2/M2GalleryEntry.h
+++ monsters2/M2GalleryEntry.h
@@ -9,11 +9,33 @@
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
-
+/**
+ Represents a gallery entry.
+ */
 @interface M2GalleryEntry : NSManagedObject
 
+/****************************************************************************
+ @name Basic properties
+ ****************************************************************************/
+
+/**
+ Gallery entry name. Entered by user.
+ */
 @property (nonatomic, retain) NSString * name;
+
+/**
+ Path to gallery entry. Selected automatically by controller that is
+ responsible for create a gallery entry.
+ */
 @property (nonatomic, retain) NSString * path;
+
+/****************************************************************************
+ @name Working with thumbnail
+ ****************************************************************************/
+
+/**
+ Serialized image of thumbnail for gallery entry.
+ */
 @property (nonatomic, retain) NSData * thumbnail;
 
 @end
