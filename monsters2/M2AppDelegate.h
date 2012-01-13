35b93fac3feab8ff5afbb2179100b91d104871e9
Vojto Rinik <vojto@rinik.net>
2012-01-13 11:07:54 +0100

    Automatically displaying about panel

---
+++ monsters2/M2AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  M2AppDelegate.h
+//  monsters2
+//
+//  Created by Rinik Vojto on 1/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface M2AppDelegate : NSObject <NSApplicationDelegate>
+
+@end
---
+++ monsters2/M2AppDelegate.m
@@ -0,0 +1,17 @@
+//
+//  M2AppDelegate.m
+//  monsters2
+//
+//  Created by Rinik Vojto on 1/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "M2AppDelegate.h"
+
+@implementation M2AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
+    [NSApp orderFrontStandardAboutPanel:nil];
+}
+
+@end
